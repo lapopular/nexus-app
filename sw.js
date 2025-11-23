a7477e30f31835319fb3d488bb6df38439c644e9
lapopular <lapopular695@gmail.com>
2025-11-22 20:21:48 -0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,6 @@
+const CACHE_NAME = 'nexus-v1';
+const urlsToCache = ['./', './index.html', './manifest.json'];
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
+});
